843a276cb866311dee22b61b2cb82570842f4b89
Nurislam Ab <you@example.com>
2020-10-08 21:20:53 +0500

    Add class name for the result wrapper

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+<<<<<<< HEAD
 const Display = ({ result }) => (
   <div>
     {result}
   </div>
 );
+=======
+const Display = ({result}) => {
+  return (
+    <div className='result-wrapper'>
+      {result}
+    </div>
+  );
+}
+>>>>>>> c7a3baa... Add class name for the result wrapper
 
 Display.defaultProps = {
   result: '0',
