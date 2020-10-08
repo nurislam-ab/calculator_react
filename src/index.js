5ee68e2ab92fd443bc9a8803cb48215a19e50c2b
Nurislam Ab <you@example.com>
2020-10-08 21:20:35 +0500

    Import reset css file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+<<<<<<< HEAD
 import App from 'App';
+=======
+import App from './components/App';
+import './reset.css';
+>>>>>>> 4eacb2e... Import reset css file
 import './index.css';
 
 ReactDOM.render(
