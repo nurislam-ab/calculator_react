5eddc44fb5b12fceaf655cecbfbd65397014c77a
Nurislam Ab <you@example.com>
2020-10-08 21:22:15 +0500

    Change Button components rendering

--- src/components/ButtonPanel.jsx
+++ src/components/ButtonPanel.jsx
@@ -15,8 +15,44 @@ const ButtonPanel = () => {
   const btnsArr = btnNamesArr.map(group => <div key={group[0]}>{btnsGroup(group)}</div>);
 
   return (
+<<<<<<< HEAD
     <div className="btn-groups-wrapper">{btnsArr}</div>
   );
 };
+=======
+    <div className='btn-groups-wrapper'>
+      <div className="group-1 btn-group">
+        <Button name="AC" color='#E0E0E0'/>
+        <Button name="+/-" color='#E0E0E0'/>
+        <Button name="%" color='#E0E0E0'/>
+        <Button name="÷"/>
+      </div>
+      <div className="group-2 btn-group">
+        <Button name="7" color='#E0E0E0'/>
+        <Button name="8" color='#E0E0E0'/>
+        <Button name="9" color='#E0E0E0'/>
+        <Button name="x"/>
+      </div>
+      <div className="group-3 btn-group">
+        <Button name="4" color='#E0E0E0'/>
+        <Button name="5" color='#E0E0E0'/>
+        <Button name="6" color='#E0E0E0'/>
+        <Button name="-"/>
+      </div>
+      <div className="group-4 btn-group">
+        <Button name="1" color='#e0e0e0'/>
+        <Button name="2" color='#E0E0E0'/>
+        <Button name="3" color='#E0E0E0'/>
+        <Button name="+"/>
+      </div>
+      <div className="group-5 btn-group">
+        <Button name="0" color='#E0E0E0' wide={true}/>
+        <Button name="." color='#E0E0E0'/>
+        <Button name="="/>
+      </div>
+    </div>
+  )
+}
+>>>>>>> 776b201... Change Button components rendering
 
 export default ButtonPanel;
