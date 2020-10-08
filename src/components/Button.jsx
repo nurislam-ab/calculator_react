28edf4d281625afbe20f188635938b0d7dee7ef8
Nurislam Ab <you@example.com>
2020-10-08 21:21:48 +0500

    Update Buttons component props with color and buttons width

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+<<<<<<< HEAD
 const Button = ({ name }) => (
   <div>
     {name}
@@ -10,6 +11,37 @@ const Button = ({ name }) => (
 Button.propTypes = {
   name: PropTypes.string,
 };
+=======
+const Button = ({name, color, wide}) => {
+  
+  
+  const styles = (btnWidth, btnColor) => {
+    const wideValue = (btnWidth) ? '2 1 0.5%' : '1';
+    
+    return ({
+      flex: wideValue,
+      backgroundColor: btnColor,
+    });
+  }
+
+  return (
+    <div className="btn" style={styles(wide, color)}>
+      {name}
+    </div>
+  );
+}
+
+Button.propTypes = {
+  name: PropTypes.string,
+  color: PropTypes.string,
+  wide: PropTypes.bool,
+}
+
+Button.defaultProps = {
+  color: '#f5913e',
+  wide: false,
+}
+>>>>>>> 22ba704... Update Buttons component props with color and buttons width
 
 Button.defaultProps = {
   name: '0',
