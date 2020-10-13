4d6c30005bd1d3b7ea31ffa83a52a89903a37989
Nurislam Ab <you@example.com>
2020-10-13 13:04:32 +0500

    Fix linter offences

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Display = ({result}) => {
-  return (
-    <div className='result-wrapper'>
-      {result}
-    </div>
-  );
-}
+const Display = ({ result }) => (
+  <div className="result-wrapper">
+    {result}
+  </div>
+);
 
 Display.defaultProps = {
   result: '0',
